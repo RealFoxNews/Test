8144d01b732187af99e535cee0a40de3c253d766
RealFoxNews <57102036+RealFoxNews@users.noreply.github.com>
2020-06-18 14:55:58 +0300

    Add files via upload

---
+++ Visitors/FillParentsVisitor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using ProgramTree;
+
+namespace SimpleLang.Visitors
+{
+    public class FillParentsVisitor : AutoVisitor
+    {
+        private readonly Stack<Node> st = new Stack<Node>();
+
+        public override void PreVisit(Node n)
+        {
+            n.Parent = st.Peek();
+            st.Push(n);
+        }
+
+        public override void PostVisit(Node n) => st.Pop();
+
+        public override void VisitStListNode(StListNode bl)
+        {
+            if (st.Count > 0)
+            {
+                bl.Parent = st.Peek();
+            }
+            st.Push(bl);
+            base.VisitStListNode(bl);
+            st.Pop();
+        }
+    }
+}
